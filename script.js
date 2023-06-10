49dd2f9bcb631bac5b135f344306860ac27db807
Abbas Askari <abbasaskari797@gmail.com>
2023-06-10 17:39:56 +0500

    ALl done

--- script.js
+++ script.js
@@ -1,43 +1,85 @@
-let choices = ["rock", "paper", "scissors"];
+let choices = ["Rock", "Paper", "Scissors"];
+let playerButtons = document.querySelectorAll(".player>button");
+let computerButtons = document.querySelectorAll(".computer>button");
+let winTag = document.querySelector(".info div");
+let explainationTag = document.querySelector(".info .explaination");
+let scoreTag = document.querySelector(".info .score");
+let allButtons = [...playerButtons, ...computerButtons];
+let computerScore = 0;
+let playerScore = 0;
+let playerChoice = "";
+let computerChoice = "";
 
-function getComputerChoice() {
+const reset = () =>
+  allButtons.forEach((button) => button.classList.remove("pressed"));
+
+playerButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    reset();
+    button.classList.add("pressed");
+    playerChoice = button.textContent;
+    // updateComputerChoice();
+    playRound();
+    // console.log(getConclusion(playerChoice, computerChoice));
+  });
+});
+
+function updateComputerChoice() {
   let random = Math.floor(Math.random() * 3);
-  return choices[random];
+  computerChoice = choices[random];
+  computerButtons[random].classList.add("pressed");
+  return computerChoice;
 }
 
-function getConclusion(playerSelection, computerSelection) {
-  let playerIndex = choices.indexOf(playerSelection.toLowerCase());
-  let computerIndex = choices.indexOf(computerSelection);
-  if ((playerIndex + 1) % 3 === computerIndex) {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
-  } else if ((computerIndex + 1) % 3 === playerIndex) {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
-  } else {
-    return "Its a draw!";
+function getConclusion() {
+  let playerIndex = choices.indexOf(playerChoice);
+  let computerIndex = choices.indexOf(computerChoice);
+  let diff = Math.abs(playerIndex - computerIndex);
+  if (diff === 2) return playerIndex > computerIndex ? -1 : 1;
+  if (diff === 1) return playerIndex > computerIndex ? 1 : -1;
+  return 0;
+}
+
+function playRound() {
+  updateComputerChoice();
+  let outCome = getConclusion();
+  if (outCome === -1) computerScore++;
+  else if (outCome === 1) playerScore++;
+  updateWinTag(outCome);
+  updateScore();
+  updateExplainTag(outCome);
+  if (computerScore === 5 || playerScore === 5) {
+    endGame();
   }
 }
 
-function playGame() {
-  let computerScore = 0;
-  let playerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt(`Enter your move: `);
-    let result = getConclusion(playerChoice, getComputerChoice());
-    if (result.startsWith("You L")) {
-      computerScore++;
-    } else if (result.startsWith("You W")) {
-      playerScore++;
-    } else {
-      i--;
-    }
-    console.log(
-      `Your score: ${playerScore}\nComputer's score: ${computerScore}\n` +
-        result
-    );
-  }
-  if (computerScore > playerScore) console.log("Sorry! you lost!");
-  else if (computerScore < playerScore) console.log("Yay! you Won!");
-  else console.log("Draw.");
+function updateWinTag(winner) {
+  if (!winner) winTag.textContent = "Draw!";
+  else winTag.textContent = `${winner > 0 ? "Player" : "Computer"} Wins`;
 }
 
-playGame();
+function updateExplainTag(winner) {
+  if (!winner) explainationTag.textContent = "";
+  else
+    explainationTag.textContent = `${
+      winner > 0 ? playerChoice : computerChoice
+    } beats ${winner < 0 ? playerChoice : computerChoice}`;
+}
+
+function updateScore() {
+  scoreTag.textContent = `${playerScore} - ${computerScore}`;
+}
+
+function endGame() {
+  reset();
+  playerButtons.forEach((button) => {
+    button.removeEventListener("click", (e) => {
+      reset();
+      button.classList.add("pressed");
+      playerChoice = button.textContent;
+      // updateComputerChoice();
+      playRound();
+      // console.log(getConclusion(playerChoice, computerChoice));
+    });
+  });
+}
